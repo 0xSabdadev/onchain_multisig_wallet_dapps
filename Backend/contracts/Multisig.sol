c10f284d11f794111813bcdd668e1ad3bfeea9e3
0xSabdadev <jasonalhilal@gmail.com>
2024-01-19 14:12:38 +0700

    setting migrations done

--- Backend/contracts/Multisig.sol
+++ Backend/contracts/Multisig.sol
@@ -2,8 +2,8 @@
 
 pragma solidity ^0.8.0;
 
-import "../../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "../../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./MultiSigFactory.sol";
 
 contract MultiSigTA {
--- Backend/contracts/Vechain.sol
+++ Backend/contracts/Vechain.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.0;
 
-import "../../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract VeChain is ERC20 {
     constructor () ERC20("VeChain", "VET") {
